948fe9a9d8a7e660fb2f2fe2f1cce903b0de704e
karthick18 <a.r.karthick@gmail.com>
2011-03-23 01:27:53 +0000

    Avoid redundant network order conversion for big endian machines
    git-svn-id: http://clovisforge.openclovis.org:8888/svn/clasp/branches/5.0@5839 8a546835-ab24-0410-9070-dd6b907a2c58

--- pkg/src/ASP/components/utils/include/clNetwork.h
+++ pkg/src/ASP/components/utils/include/clNetwork.h
@@ -12,8 +12,8 @@ static const int __endianTest = 1;
 
 static  __inline__ ClInt64T clHtonl64(ClInt64T v)
 {
-    ClUint32T v1 = htonl(v & 0xffffffffU);
-    ClUint32T v2 = htonl( (v >> 32) & 0xffffffffU);
+    ClUint32T v1 = v & 0xffffffffU;
+    ClUint32T v2 = (v >> 32) & 0xffffffffU;
     if(__endianType < 0)
     {
         __endianType = (*(char*)&__endianTest == 1 )  ? 0 : 1;
@@ -26,6 +26,8 @@ static  __inline__ ClInt64T clHtonl64(ClInt64T v)
         v1 ^= v2;
         v2 ^= v1;
         v1 ^= v2;
+        v1 = htonl(v1);
+        v2 = htonl(v2);
     }
     v = ((ClInt64T)v2 << 32) | v1;
     return v;
@@ -33,8 +35,8 @@ static  __inline__ ClInt64T clHtonl64(ClInt64T v)
 
 static __inline__ ClInt64T clNtohl64(ClInt64T v)
 {
-    ClUint32T v1 = ntohl(v & 0xffffffffU);
-    ClUint32T v2 = ntohl( (v >> 32) & 0xffffffffU);
+    ClUint32T v1 = v & 0xffffffffU;
+    ClUint32T v2 = (v >> 32) & 0xffffffffU;
     if(__endianType < 0)
     {
         __endianType = ( *(char*)&__endianTest == 1 ) ? 0 : 1;
@@ -44,6 +46,8 @@ static __inline__ ClInt64T clNtohl64(ClInt64T v)
         v1 ^= v2;
         v2 ^= v1;
         v1 ^= v2;
+        v1 = ntohl(v1);
+        v2 = ntohl(v2);
     }
     v = ((ClInt64T)v2 << 32) | v1;
     return v;
