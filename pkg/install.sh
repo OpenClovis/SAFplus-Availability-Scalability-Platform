3a26dfa584e99500f28421ab7a099b6fe286f2d0
hoangle <hoang.le@openclovis.com>
2012-05-24 01:40:36 +0000

    Update openhpi thirdparty for oc
    git-svn-id: http://clovisforge.openclovis.org:8888/svn/clasp/branches/5.0@6936 8a546835-ab24-0410-9070-dd6b907a2c58

--- pkg/install.sh
+++ pkg/install.sh
@@ -509,7 +509,7 @@ GDBM_VERSION=gdbm-1.8.0
 GLIB_VERSION=glib-2.12.6
 GLIBC_VERSION=glibc-2.3.5
 OPENHPI_VERSION=openhpi-2.16.0.oc
-OPENHPI_PKG=openhpi-2.14.0.oc.tar.gz
+OPENHPI_PKG=openhpi-2.16.0.oc.tar.gz
 if [ "$CUSTOM_OPENHPI" == "1" ]; then
     OPENHPI_PKG=$CUSTOM_OPENHPI_PKG
 fi
