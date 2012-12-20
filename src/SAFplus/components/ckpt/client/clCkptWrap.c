c402167605a54ee1dfe193182e8510c462481a4b
A R Karthick <a.r.karthick@gmail.com>
2012-12-19 16:14:21 -0800

    Only handle CKPT_ERR_NOT_EXIST in case the active replica address was transitioning or uninitialized. Also retry only on CL_ERR_NOT_EXIST since with multi-xport mode, the link map for the node would be deleted resulting in send/reachability issues during node leaves in which case we may have to retry

--- src/SAFplus/components/ckpt/client/clCkptWrap.c
+++ src/SAFplus/components/ckpt/client/clCkptWrap.c
@@ -1428,8 +1428,9 @@ static ClBoolT clCkptHandleTypicalErrors(ClRcT rc, ClCkptHdlT ckptHdl,ClIocNodeA
   ClRcT retCode;
   if(  (CL_GET_ERROR_CODE(rc) == CL_IOC_ERR_COMP_UNREACHABLE) ||
        (CL_IOC_ERR_HOST_UNREACHABLE == CL_GET_ERROR_CODE(rc)) || 
-       ((CL_GET_ERROR_CODE(rc) == CL_ERR_NOT_EXIST) && nodeAddr && *nodeAddr == CL_CKPT_UNINIT_VALUE))
-
+       (rc == CL_CKPT_ERR_NOT_EXIST && nodeAddr && *nodeAddr == CL_CKPT_UNINIT_VALUE)
+       ||
+       rc == CL_ERR_NOT_EXIST)
     {
       /* 
        * Maybe the active address has changed and this client 
