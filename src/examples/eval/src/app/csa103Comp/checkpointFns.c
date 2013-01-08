5fe36430927798db290bbe961ca6200be8a9b89b
Andrew Stone <stone@openclovis.com>
2013-01-08 12:25:16 -0500

    fix checkpointing evaluation

--- src/examples/eval/src/app/csa103Comp/checkpointFns.c
+++ src/examples/eval/src/app/csa103Comp/checkpointFns.c
@@ -2,135 +2,85 @@
 #include <clLogApi.h>
 #include <saAmf.h>
 #include <saCkpt.h>
-
-ClRcT checkpoint_initialize(void);
-ClRcT checkpoint_finalize(void);
-ClRcT checkpoint_write_seq(ClUint32T);
-ClRcT checkpoint_read_seq(ClUint32T*);
-ClRcT checkpoint_replica_activate(void);
+#include "checkpointFns.h"
 
 extern ClBoolT unblockNow;
 extern ClLogStreamHandleT gEvalLogStream;
 extern SaAmfHAStateT  ha_state;
 extern ClUint32T      seq;
 
-#define CKPT_NAME     "csa103Ckpt"  /* Checkpoint name for this application  */
-SaCkptHandleT  ckpt_svc_handle; /* Checkpointing service handle       */
-SaCkptCheckpointHandleT ckpt_handle;  /* Checkpoint handle        */
-#define CKPT_SID_NAME "1"           /* Checkpoint section id                 */
-SaCkptSectionIdT ckpt_sid = { /* Section id for checkpoints           */
-        (SaUint16T)sizeof(CKPT_SID_NAME)-1,
-        (SaUint8T*)CKPT_SID_NAME
-};
+static SaInvocationT syncCount=0;
 
 #define clprintf(severity, ...)   clAppLog(gEvalLogStream, severity, 10,CL_LOG_AREA_UNSPECIFIED, CL_LOG_CONTEXT_UNSPECIFIED,__VA_ARGS__)
 
-#if 0
-void csa103TestDispatcher(ClTaskPoolHandleT *pTestHandle)
+void checkpointOpenCompleted(SaInvocationT invocation, SaCkptCheckpointHandleT checkpointHandle, SaAisErrorT error)
 {
-    ClRcT rc = CL_OK;
-    rc = clTaskPoolCreate(pTestHandle, 1, NULL, NULL);
-    CL_ASSERT(rc == CL_OK);
-    clTaskPoolRun(*pTestHandle, csa103CkptTest, NULL);
+clprintf (CL_LOG_SEV_INFO, "Checkpoint open completed");
 }
-#endif
 
-ClRcT checkpoint_initialize()
+
+void checkpointSynchronizeCompleted (SaInvocationT invocation,SaAisErrorT error)
+{
+    /* Just log that the sync completed but not too often */
+    if ((invocation%10)==0) clprintf (CL_LOG_SEV_INFO, "Checkpoint synchronize [%llu] completed ",invocation);
+}
+
+
+SaAisErrorT checkpoint_initialize(void)
 {
     SaAisErrorT      rc = CL_OK;
     SaVersionT ckpt_version = {'B', 1, 1};
     SaNameT    ckpt_name = { strlen(CKPT_NAME), CKPT_NAME };
-    ClUint32T  seq_no;
-    SaCkptCheckpointCreationAttributesT create_atts = {
-        .creationFlags     = SA_CKPT_WR_ACTIVE_REPLICA_WEAK |
-                             SA_CKPT_CHECKPOINT_COLLOCATED,
-        .checkpointSize    = sizeof(ClUint32T),
-        .retentionDuration = (ClTimeT)10, 
-        .maxSections       = 2, // two sections 
-        .maxSectionSize    = sizeof(ClUint32T),
-        .maxSectionIdSize  = (ClSizeT)64
-    };
+    SaCkptCallbacksT callbacks;
+    SaCkptCheckpointCreationAttributesT attrs;
 
-    SaCkptSectionCreationAttributesT section_atts = {
-        .sectionId = &ckpt_sid,
-        .expirationTime = SA_TIME_END
-    };
-	  
-    clprintf(CL_LOG_SEV_INFO,"checkpoint_initialize");
-    /* Initialize checkpointing service instance */
-    rc = saCkptInitialize(&ckpt_svc_handle,	/* Checkpoint service handle */
-						  NULL,			    /* Optional callbacks table */
-						  &ckpt_version);   /* Required verison number */
+    /* Set up the checkpoint configuration */
+    attrs.creationFlags     = SA_CKPT_WR_ACTIVE_REPLICA_WEAK | SA_CKPT_CHECKPOINT_COLLOCATED;        
+    attrs.checkpointSize    = sizeof(ClUint32T);
+    attrs.retentionDuration = (ClTimeT) 10;        /* How long to keep the checkpoint around even if noone has it open */
+    attrs.maxSections       = 2;                   /* Max number of 'rows' in the table */
+    attrs.maxSectionSize    = sizeof(ClUint32T);   /* Max 'row' size */
+    attrs.maxSectionIdSize  = (ClSizeT)64;         /* Max 'row' identifier size */
+
+    /* These callbacks are only called when an Async style function call completes */
+    callbacks.saCkptCheckpointOpenCallback        = checkpointOpenCompleted;
+    callbacks.saCkptCheckpointSynchronizeCallback = checkpointSynchronizeCompleted;
+        
+    clprintf(CL_LOG_SEV_INFO,"Checkpoint Initialize");
+
+        
+    /* Initialize checkpointing library */
+    rc = saCkptInitialize(&ckptLibraryHandle,	/* Checkpoint service handle */
+                          &callbacks,			    /* Optional callbacks table */
+                          &ckpt_version);   /* Required verison number */
     if (rc != SA_AIS_OK)
     {
         clprintf(CL_LOG_SEV_ERROR,"Failed to initialize checkpoint service with rc [%#x]", rc);
         return rc;
     }
-    clprintf(CL_LOG_SEV_INFO,"Checkpoint service initialized (handle=0x%llx)", ckpt_svc_handle);
-    
-    //
-    // Create the checkpoint for read and write.
-    rc = saCkptCheckpointOpen(ckpt_svc_handle,      // Service handle
+    clprintf(CL_LOG_SEV_INFO,"Checkpoint service initialized (handle=0x%llx)", ckptLibraryHandle);
+        
+    /* Create the checkpoint table for read and write. */
+    rc = saCkptCheckpointOpen(ckptLibraryHandle,      // Service handle
                               &ckpt_name,         // Checkpoint name
-                              &create_atts,       // Optional creation attr.
+                              &attrs,       // Optional creation attr.
                               (SA_CKPT_CHECKPOINT_READ |
                                SA_CKPT_CHECKPOINT_WRITE |
                                SA_CKPT_CHECKPOINT_CREATE),
-                              (SaTimeT)-1,        // No timeout
+                              (SaTimeT)SA_TIME_MAX,        // No timeout
                               &ckpt_handle);      // Checkpoint handle
 
     if (rc != SA_AIS_OK)
     {
         clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to open checkpoint",rc);
-        (void)saCkptFinalize(ckpt_svc_handle);
         return rc;
     }
     clprintf(CL_LOG_SEV_INFO,"Checkpoint opened (handle=0x%llx)", ckpt_handle);
-    
-    /*
-     * Try to create a section so that updates can operate by overwriting
-     * the section over and over again.
-     * If subsequent processes come through here, they will fail to create
-     * the section.  That is OK, even though it will cause an error message
-     * If the section create fails because the section is already there, then
-     * read the sequence number
-     */
-    // Put data in network byte order
-    seq_no = htonl(seq);
-
-    // Creating the section
-    checkpoint_replica_activate();
-    rc = saCkptSectionCreate(ckpt_handle,           // Checkpoint handle
-                             &section_atts,         // Section attributes
-                             (SaUint8T*)&seq_no,    // Initial data
-                             (SaSizeT)sizeof(seq_no)); // Size of data
-    if (rc != SA_AIS_OK && (CL_GET_ERROR_CODE(rc) != SA_AIS_ERR_EXIST))
-    {
-        clprintf(CL_LOG_SEV_ERROR,"Failed to create checkpoint section");
-        (void)saCkptCheckpointClose(ckpt_handle);
-        (void)saCkptFinalize(ckpt_svc_handle);
-        return rc;
-    }
-    else if (rc != SA_AIS_OK && (CL_GET_ERROR_CODE(rc) == SA_AIS_ERR_EXIST))
-    {
-        rc = checkpoint_read_seq(&seq);
-        if (rc != CL_OK)
-        {
-            clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to read checkpoint section", rc);
-            (void)saCkptCheckpointClose(ckpt_handle);
-            (void)saCkptFinalize(ckpt_svc_handle);
-            return rc;
-        }
-    }
-    else
-    {
-        clprintf(CL_LOG_SEV_INFO,"Section created");
-    }
-
-    return CL_OK;
+    return rc;
 }
 
-ClRcT checkpoint_finalize(void)
+
+void checkpoint_finalize(void)
 {
     SaAisErrorT rc;
 
@@ -139,15 +89,14 @@ ClRcT checkpoint_finalize(void)
     {
         clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to close checkpoint handle 0x%llx", rc, ckpt_handle);
     }
-    rc = saCkptFinalize(ckpt_svc_handle);
+    rc = saCkptFinalize(ckptLibraryHandle);
     if (rc != SA_AIS_OK)
     {
         clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to finalize checkpoint", rc);
     }
-    return CL_OK;
 }
 
-ClRcT checkpoint_write_seq(ClUint32T seq)
+SaAisErrorT checkpoint_write_seq(ClUint32T seq)
 {
     SaAisErrorT rc = SA_AIS_OK;
     ClUint32T seq_no;
@@ -156,31 +105,51 @@ ClRcT checkpoint_write_seq(ClUint32T seq)
     seq_no = htonl(seq);
     
     /* Write checkpoint */
-    retry:
-    rc = saCkptSectionOverwrite(ckpt_handle,
-                                &ckpt_sid,
-                                &seq_no,
-                                sizeof(ClUint32T));
+    rc = saCkptSectionOverwrite(ckpt_handle, &ckpt_sid, &seq_no, sizeof(ClUint32T));
     if (rc != SA_AIS_OK)
     {
-        clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to write to section", rc);
-        if(rc == SA_AIS_ERR_NOT_EXIST)
-            rc = checkpoint_replica_activate();
-        if(rc == CL_OK) goto retry;
+        /* The error SA_AIS_ERROR_NOT_EXIST can occur either because we are not the active replica OR
+           if the overwrite failed because the section did not exist then try to create the section.
+
+           But in this case we know our application will not attempt to write unless it has set itself as
+           the active replica so the problem MUST be a missing section.
+        */
+        if ((rc == 0x1000a) || (rc == SA_AIS_ERR_NOT_EXIST))
+        {
+            SaCkptSectionCreationAttributesT    section_cr_attr;
+            
+            /* Expiration time for section can also be varied
+             * once parameters are read from a configuration file
+             */
+            section_cr_attr.expirationTime = SA_TIME_END;
+            section_cr_attr.sectionId = &ckpt_sid;
+            rc = saCkptSectionCreate(ckpt_handle,&section_cr_attr,(SaUint8T*) &seq_no, sizeof(ClUint32T));            
+        }
+        if (rc != SA_AIS_OK)
+        {
+            clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to write to section", rc);
+        }
     }
-    else 
+
+    if (rc == SA_AIS_OK)
     {
         /*
          * Synchronize the checkpoint to all the replicas.
          */
+#if 0          /* Synchronous synchronize example */
         rc = saCkptCheckpointSynchronize(ckpt_handle, SA_TIME_END );
         if (rc != SA_AIS_OK)
         {
             clprintf(CL_LOG_SEV_ERROR,"Failed [0x%x] to synchronize the checkpoint", rc);
         }
+#else
+        rc = saCkptCheckpointSynchronizeAsync(ckpt_handle,syncCount);
+        syncCount++;
+        
+#endif        
     }
 
-    return CL_OK;
+    return rc;
 }
 
 ClRcT checkpoint_read_seq(ClUint32T *seq)
