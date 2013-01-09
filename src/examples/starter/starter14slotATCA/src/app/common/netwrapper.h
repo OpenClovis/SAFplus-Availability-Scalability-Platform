f97522fa7310d6c44c159f31974f155f86cbd83b
Hoang Le <hoang.le@openclovis.com>
2013-01-09 13:15:01 +0700

    Fixed issue on model example starter14slotATCA

--- src/examples/starter/starter14slotATCA/src/app/common/netwrapper.h
+++ src/examples/starter/starter14slotATCA/src/app/common/netwrapper.h
@@ -1,3 +1,4 @@
+#include "saAmf.h"
 
 enum
   {
@@ -12,7 +13,8 @@ typedef struct
 } VirtualIpAddress;
 
 void  AddRemVirtualAddress(const char *cmd,const VirtualIpAddress* vip);
-void  GetVirtualAddressInfo(ClAmsCSIDescriptorT* csiDescriptor, VirtualIpAddress* vip);
+void  GetVirtualAddressInfo(SaAmfCSIDescriptorT* csiDescriptor, VirtualIpAddress* vip);
+void  GetVirtualAddressInfoAsp(ClAmsCSIDescriptorT* csiDescriptor, VirtualIpAddress* vip);
 ClRcT SendArp(const char* host, const char* dev);
 ClRcT HostToIp(const char* myHost, unsigned int* ip);
 ClRcT DevToMac(const char* dev, char mac[6]);
