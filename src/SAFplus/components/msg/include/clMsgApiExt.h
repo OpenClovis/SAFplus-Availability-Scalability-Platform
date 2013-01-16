557a7d6a79b8b170d07506278e95a18fcab9593c
Cang Tran <cangtran@localhost.localdomain>
2013-01-16 16:36:14 +0700

    Integration of consistent hash implementation prototype into msg server. Add 4 new functions: clMsgQueueGroupHashInit, clMsgQueueGroupHashFinalize, clMsgQueueGroupSendWithKeySynch and clMsgQueueGroupSendWithKeyAsync.

--- src/SAFplus/components/msg/include/clMsgApiExt.h
+++ src/SAFplus/components/msg/include/clMsgApiExt.h
@@ -16,6 +16,32 @@ extern ClRcT clMsgDispatchQueueDeregister(SaMsgQueueHandleT queueHandle);
 
 extern ClRcT clMsgQueuePersistRedundancy(const SaNameT *queue, const SaNameT *node);
 
+/**
+ * Initialize a consistent hash ring for a MSG queue group
+ * and allocate a MsgQueueGroupHashesT to store the hash for receiver lookup
+ */
+extern ClRcT clMsgQueueGroupHashInit(const SaNameT *group, ClInt32T nodes, ClInt32T hashesPerNode);
+
+/**
+ * Free memory allocated for consistent hash of a group
+ */
+extern ClRcT clMsgQueueGroupHashFinalize(const SaNameT *group);
+
+/**
+ * Lookup and message to the receiver in the queue group that map with the key
+ */
+extern SaAisErrorT clMsgQueueGroupSendWithKeySynch(SaMsgHandleT msgHandle,
+                                                   const SaNameT *group, SaMsgMessageT *message,
+                                                   ClCharT *key, ClInt32T keylen,
+                                                   SaTimeT timeout);
+extern SaAisErrorT clMsgQueueGroupSendWithKeyAsync(SaMsgHandleT msgHandle,
+                                                   SaInvocationT invocation,
+                                                   const SaNameT *group, SaMsgMessageT *message,
+                                                   ClCharT *key, ClInt32T keylen,
+                                                   SaMsgAckFlagsT ackFlags);
+
+
+
 #ifdef __cplusplus
 }
 #endif
