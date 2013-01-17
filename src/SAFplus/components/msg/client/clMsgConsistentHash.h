27f94e8fa39f895f3b38571adc9b3da7f56eb2ca
Cang Tran <cangtran@localhost.localdomain>
2013-01-17 16:03:13 +0700

    The msgQueueGroupHashInit will not be an exposed api. It is called on clMsgInitialize phase

---
+++ src/SAFplus/components/msg/client/clMsgConsistentHash.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2002-2012 OpenClovis Solutions Inc.  All Rights Reserved.
+ *
+ * This file is available  under  a  commercial  license  from  the
+ * copyright  holder or the GNU General Public License Version 2.0.
+ *
+ * The source code for  this program is not published  or otherwise
+ * divested of  its trade secrets, irrespective  of  what  has been
+ * deposited with the U.S. Copyright office.
+ *
+ * This program is distributed in the  hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied  warranty  of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * For more  information, see  the file  COPYING provided with this
+ * material.
+ */
+/*******************************************************************************
+ * ModuleName  : message
+ * File        : clMsgConsistentHash.h
+ *******************************************************************************/
+
+#include <clCommon.h>
+#include <clCommonErrors.h>
+
+#define __MSG_QUEUE_GROUP_NODES (16)
+#define __MSG_QUEUE_GROUP_HASHES_PER_NODE (100)
+#define __MSG_QUEUE_GROUP_HASHES ( __MSG_QUEUE_GROUP_NODES * __MSG_QUEUE_GROUP_HASHES_PER_NODE )
+
+/*
+ * Initialize a consistent hash ring for a MSG queue group
+ * and allocate a MsgQueueGroupHashesT to store the hash for receiver lookup
+ */
+ClRcT clMsgQueueGroupHashInit(ClInt32T nodes, ClInt32T hashesPerNode);
+
+/*
+ * Free memory allocated for consistent hash of a group
+ */
+ClRcT clMsgQueueGroupHashFinalize();
