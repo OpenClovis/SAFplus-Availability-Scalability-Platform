b7ddf85aca67086e78f9729a9af3e7dba489319f
A R Karthick <a.r.karthick@gmail.com>
2013-02-06 13:27:47 -0800

    Do the sanity check of the name length before unmarshalling the namet value char array. Makes it for a better armor than doing it after the unmarshall even though marshall ensures that the length doesn't exceed the namet limit

--- src/SAFplus/components/utils/client/xdrClNameTUnMarshall.c
+++ src/SAFplus/components/utils/client/xdrClNameTUnMarshall.c
@@ -45,13 +45,14 @@ ClRcT clXdrUnmarshallClNameT(ClBufferHandleT msg , void* pGenVar)
         return rc;
     }
 
+   if(pVar->length >= CL_MAX_NAME_LENGTH)
+        pVar->length = CL_MAX_NAME_LENGTH - 1;
+ 
     rc = clXdrUnmarshallArrayClCharT(msg, pVar->value, pVar->length);
     if (CL_OK != rc)
     {
         return rc;
     }
-    if(pVar->length >= CL_MAX_NAME_LENGTH)
-        pVar->length = CL_MAX_NAME_LENGTH - 1;
     
     pVar->value[pVar->length] = 0;
 
