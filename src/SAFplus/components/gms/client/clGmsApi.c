62fc395baa02481aa4427ff7200bf63f2b6a325e
A R Karthick <a.r.karthick@gmail.com>
2013-02-06 14:12:27 -0800

    Do a gms thread specific key create and delete only once instead of doing it everytime on call of clGmsInitialize/Finalize. Wish pthread_once_init had osal support but this should be good enough as a barrier

--- src/SAFplus/components/gms/client/clGmsApi.c
+++ src/SAFplus/components/gms/client/clGmsApi.c
@@ -68,6 +68,7 @@ ClHandleDatabaseHandleT handle_database;
  * invocation of the callback.
  */
 ClUint32T       clGmsPrivateDataKey = 0;
+static ClBoolT  gClTaskKeyCreated = CL_FALSE;
 
 /* Flag to show if library is initialized */
 static ClBoolT lib_initialized = CL_FALSE;
@@ -221,6 +222,29 @@ error_exit:
     return rc;
 }
 
+static ClRcT gmsTaskKeyCreate(void)
+{
+    ClRcT rc = CL_OK;
+    if(gClTaskKeyCreated) return CL_OK;
+    rc = clOsalTaskKeyCreate(&clGmsPrivateDataKey, NULL);
+    if (rc != CL_OK)
+    {
+        return rc;
+    }
+    gClTaskKeyCreated = CL_TRUE;
+    return rc;
+}
+
+static ClRcT gmsTaskKeyDelete(void)
+{
+    ClRcT rc = CL_OK;
+    if(!gClTaskKeyCreated) return CL_OK;
+    gClTaskKeyCreated = CL_FALSE;
+    /* Delete the key for thread specific data set */
+    rc = clOsalTaskKeyDelete(clGmsPrivateDataKey);
+    return rc;
+}
+
 
 /******************************************************************************
  * EXPORTED API FUNCTIONS
@@ -238,7 +262,7 @@ ClRcT clGmsInitialize(
     ClGmsClientInitRequestT req = {{0}};
     ClGmsClientInitResponseT *res = NULL;
 
-    /* Step 0: Ceck readyness of library */
+    /* Step 0: Check readiness of library */
 
     rc = check_lib_init();
     if (rc != CL_OK)
@@ -313,11 +337,10 @@ ClRcT clGmsInitialize(
            sizeof(ClGmsGroupNotificationBufferT));
 
     /* Create the key for thread specific data set */
-    rc = clOsalTaskKeyCreate(&clGmsPrivateDataKey,NULL);
-    if (rc != CL_OK)
+    rc = gmsTaskKeyCreate();
+    if(rc != CL_OK)
     {
-        CL_DEBUG_PRINT(CL_DEBUG_ERROR,
-                ("clOsalTaskKeyCreate failed with rc 0x%x\n",rc));
+        clLogError("TASK", "KEY", "TaskKeyCreate returned [%#x]", rc);
     }
 
     /* Step 6: Decrement handle use count and return */
@@ -374,11 +397,10 @@ ClRcT clGmsFinalize(
 	clGmsMutexDelete(gms_instance_ptr->response_mutex);
 
     /* Delete the key for thread specific data set */
-    rc = clOsalTaskKeyDelete(clGmsPrivateDataKey);
+    rc = gmsTaskKeyDelete();
     if (rc != CL_OK)
     {
-        CL_DEBUG_PRINT(CL_DEBUG_ERROR,
-                ("clOsalTaskKeyDelete failed with rc 0x%x\n",rc));
+        clLogError("TASK", "KEY", "TaskKeyDelete returned [%#x]", rc);
     }
 
 	if ((clHandleDestroy(handle_database, gmsHandle)) != CL_OK)
