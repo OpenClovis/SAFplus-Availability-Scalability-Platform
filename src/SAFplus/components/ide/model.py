bbea6de3af445d7170534c18d900b58929949332
Andrew Stone <stone@openclovis.com>
2014-11-25 18:00:59 -0500

    WIP quick example structure for the entity definition system

---
+++ src/SAFplus/components/ide/model.py
@@ -0,0 +1,14 @@
+
+class Model:
+  def __init__(self):
+    pass
+
+  def load(self, fileOrString):
+    """Load an XML representation of the model"""
+    pass
+
+  def store(self):
+    """Returns an XML string that defines the IDE Model, for saving to disk"""
+    pass
+
+  
