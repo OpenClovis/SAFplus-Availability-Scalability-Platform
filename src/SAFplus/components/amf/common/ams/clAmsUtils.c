0ed5f24b9fd4b2226599b212ae6054662a035645
Hoang Le <hoang.le@openclovis.com>
2014-12-17 10:23:17 +0700

    Remove log msg to Node's log from AMS client lib

--- src/SAFplus/components/amf/common/ams/clAmsUtils.c
+++ src/SAFplus/components/amf/common/ams/clAmsUtils.c
@@ -61,8 +61,6 @@ void clAmsLogMsgClient( const ClUint32T level, char *buffer )
 {
     ClLogSeverityT severity = (ClLogSeverityT)level;
     if(!buffer) return;
-    clOsalPrintf (buffer);
-    if(buffer[strlen(buffer)-1] != '\n') clOsalPrintf("\n");
     clLogWrite(CL_LOG_HANDLE_APP, severity, NULL, buffer);
     clAmsFreeMemory (buffer);
 }
