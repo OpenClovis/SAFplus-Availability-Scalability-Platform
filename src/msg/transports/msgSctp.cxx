cb3116878d610e4712f0dcbf9dc7cf32c2b9f76c
hungta <hungtath99@gmail.com>
2015-03-19 16:01:05 +0700

    Correct msgSctp

--- src/msg/transports/msgSctp.cxx
+++ src/msg/transports/msgSctp.cxx
@@ -1,49 +1,41 @@
 //#define _GNU_SOURCE // needed so that sendmmsg is available
 
-#include <clMsgTransportPlugin.hxx>
-#include <sys/socket.h>
+#include <clMsgUdp.hxx>
 #include <sys/types.h>
 #include <netinet/ip.h>
 #include <netinet/sctp.h>
-#include "clPluginHelper.hxx"
 #include <clCommon.hxx>  // For exceptions
+#include <clIocConfig.h>
+#include "clPluginHelper.hxx"
+#include <boost/unordered_map.hpp>
 
+typedef boost::unordered_map<uint_t, int> NodeIDSocketMap;
 
 namespace SAFplus
-{
-
+{ 
   class Sctp: public Udp
   {
   public:
-    Sctp(): Udp()
+    Sctp()
     {
     }
     virtual MsgTransportConfig& initialize(MsgPool& msgPool);
     virtual MsgSocket* createSocket(uint_t port);
-#if 0
-  public:
-    in_addr_t netAddr;
-    Udp(); 
-
-    virtual MsgTransportConfig& initialize(MsgPool& msgPool);
-
-    virtual MsgSocket* createSocket(uint_t port);
-    virtual void deleteSocket(MsgSocket* sock);
-#endif
   };
 
-  class SctpSocket:public UdpSocket
+  class SctpSocket: public UdpSocket
   {
   public:
-    SctpSocket(uint_t port,MsgPool* pool,MsgTransportPlugin_1* transport);
-    virtual ~SctpSocket();
-#if 0
+    SctpSocket(uint_t port,MsgPool* pool,MsgTransportPlugin_1* transport);    
     virtual void send(Message* msg);
     virtual Message* receive(uint_t maxMsgs,int maxDelay=-1);
     virtual void flush();
-    protected:
-    int sock;
-#endif
+    virtual ~SctpSocket();
+  protected:
+    NodeIDSocketMap clientSockMap; //TODO in case node failure: if a node got failure, this must be notified so that
+    // the item associated with it in the map must be deleted too, if not, the associated socket may be invalid because of its server failure
+    int getClientSocket(uint_t nodeID, uint_t port);
+    int openClientSocket(uint_t nodeID, uint_t port);    
   };
 
 
@@ -78,18 +70,14 @@ namespace SAFplus
     return config;
   }
 
-  MsgSocket* Udp::createSocket(uint_t port)
+  MsgSocket* Sctp::createSocket(uint_t port)
   {
     if (port >= SAFplusI::SctpTransportNumPorts) return NULL;
-    return new SctpSocket(port, msgPool,this);
+    return new SctpSocket(port, msgPool,this); // open a listen socket
   }
+
   SctpSocket::SctpSocket(uint_t pt,MsgPool*  pool,MsgTransportPlugin_1* xp)
-  {
-
-    transportStartPort = SAFplusI::SctpTransportStartPort;
-    transportMaxMsg = SAFplusI::SctpTransportMaxMsg;
-    transportMaxFragments = SAFplusI::SctpTransportMaxFragments;
-
+  {    
     port = pt;
     msgPool = pool;
     transport = xp;
@@ -162,106 +150,220 @@ namespace SAFplus
     {
       int err = errno;
       throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
-     }
-   }
-#if 0    
-   void UdpSocket::send(Message* next)
-      {
-      mmsghdr msgvec[SAFplusI::UdpTransportMaxMsg];  // We are doing this for perf so we certainly don't want to new or malloc it!
-      struct iovec iovecBuffer[SAFplusI::UdpTransportMaxFragments];
-      mmsghdr* curvec = &msgvec[0];
-      int msgCount = 0;
-      int fragCount= 0;  // frags in this message
-      int totalFragCount = 0; // total number of fragments
-      Message* msg;
-      MsgFragment* nextFrag;
-      MsgFragment* frag;
-      struct sockaddr_in to[SAFplusI::UdpTransportMaxMsg];
+    } 
+    if(listen(sock, CL_IOC_MAX_NODES) < 0)
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+  }
+
+  int SctpSocket::openClientSocket(uint_t nodeID, uint_t port)
+  {
+    int sock,ret;    
+    char address[16];    
+    sctp_assoc_t id;
+    struct sockaddr_in addr;
+    struct sctp_initmsg initmsg;
+    struct sctp_event_subscribe events;
+    struct sctp_paddrparams heartbeat;
+    struct sctp_rtoinfo rtoinfo;
+
+    memset(&initmsg,    0,   sizeof(struct sctp_initmsg));
+    memset(&addr,       0,   sizeof(struct sockaddr_in));
+    memset(&events,     1,   sizeof(struct sctp_event_subscribe));
+    memset(&heartbeat,  0,   sizeof(struct sctp_paddrparams));
+    memset(&rtoinfo,    0, sizeof(struct sctp_rtoinfo));
+
+    //TODO: transform the nodeID to IP address and set it to variable address
+
+    addr.sin_family = AF_INET;
+    inet_aton(address, &(addr.sin_addr));
+    addr.sin_port = htons(port);
+
+    initmsg.sinit_num_ostreams = SAFplusI::SctpMaxStream;
+    initmsg.sinit_max_instreams = SAFplusI::SctpMaxStream;
+    initmsg.sinit_max_attempts = SAFplusI::SctpMaxStream;
+
+    heartbeat.spp_flags = SPP_HB_ENABLE;
+    heartbeat.spp_hbinterval = 5000;
+    heartbeat.spp_pathmaxrxt = 1;
+
+    rtoinfo.srto_max = 2000;
+
+    if((ret = (sock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP))) < 0)    
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+    /*Configure Heartbeats*/
+    if((ret = setsockopt(sock, SOL_SCTP, SCTP_PEER_ADDR_PARAMS , &heartbeat, sizeof(heartbeat))) != 0)
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+
+    /*Set rto_max*/
+    if((ret = setsockopt(sock, SOL_SCTP, SCTP_RTOINFO , &rtoinfo, sizeof(rtoinfo))) != 0)
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+
+    /*Set SCTP Init Message*/
+    if((ret = setsockopt(sock, SOL_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg))) != 0)
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+
+    /*Enable SCTP Events*/
+    if((ret = setsockopt(sock, SOL_SCTP, SCTP_EVENTS, (void *)&events, sizeof(events))) != 0)
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+
+    /*Get And Print Heartbeat Interval*/
+    /*i = (sizeof heartbeat);
+    getsockopt(sock, SOL_SCTP, SCTP_PEER_ADDR_PARAMS, &heartbeat, (socklen_t*)&i);
+
+    printf("Heartbeat interval %d\n", heartbeat.spp_hbinterval);*/
+
+    /*Connect to server*/
+    if(((ret = connect(sock, (struct sockaddr*)&addr, sizeof(struct sockaddr)))) < 0)
+    {
+      int err = errno;
+      throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+    }
+    return sock;
+  }
+
+  int SctpSocket::getClientSocket(uint_t nodeID, uint_t port)
+  {
+    NodeIDSocketMap::iterator contents = clientSockMap.find(nodeID);
+    if (contents != clientSockMap.end()) // Socket connected to this NodeID has been opened
+    {
+      return contents->second;
+    }
+    int newSock = openClientSocket(nodeID, port);
+    clientSockMap[nodeID] = newSock;
+    return newSock;
+  }
+  
+  void SctpSocket::send(Message* origMsg)
+  {
+    mmsghdr msgvec[SAFplusI::SctpTransportMaxMsg];  // We are doing this for perf so we certainly don't want to new or malloc it!
+    struct iovec iovecBuffer[SAFplusI::SctpTransportMaxFragments];
+    mmsghdr* curvec = &msgvec[0];
+    int msgCount = 0;
+    int fragCount= 0;  // frags in this message
+    int totalFragCount = 0; // total number of fragments    
+    Message* msg;
+    Message* next = origMsg;
+    MsgFragment* nextFrag;
+    MsgFragment* frag;
       //in_addr inetAddr;
       //inet_aton("127.0.0.1",&inetAddr);  // TODO create the real address
       //inetAddr.s_addr = htonl(((Udp*)transport)->netAddr | 
+    do 
+    {
+      msg = next;
+      next = msg->nextMsg;  // Save the next message so we use it next
 
-      do {
-        msg = next;
-        next = msg->nextMsg;  // Save the next message so we use it next
+      struct iovec *msg_iov = iovecBuffer + totalFragCount;
+      struct iovec *curIov = msg_iov;
+      fragCount=0;
+      nextFrag = msg->firstFragment;
+      do 
+      {
+        frag = nextFrag;
+        nextFrag = frag->nextFragment;
+        // Fill the iovector with messages
+        curIov->iov_base = frag->data();     //((char*)frag->buffer)+frag->start;
+        assert((frag->len > 0) && "The UDP protocol allows sending zero length messages but I think you forgot to set the fragment len field.");
+        curIov->iov_len = frag->len;
 
-        struct iovec *msg_iov = iovecBuffer + totalFragCount;
-        struct iovec *curIov = msg_iov;
-        fragCount=0;
-        nextFrag = msg->firstFragment;
-        do {
-          frag = nextFrag;
-          nextFrag = frag->nextFragment;
+        fragCount++;
+        totalFragCount++;
+        curIov++;
+       }
+       while(nextFrag);
+      assert(msgCount < SAFplusI::SctpTransportMaxMsg);  // or stack buffer will be exceeded      
+      curvec->msg_hdr.msg_controllen = 0;
+      curvec->msg_hdr.msg_control = NULL;
+      curvec->msg_hdr.msg_flags = 0;
+      curvec->msg_hdr.msg_namelen = sizeof (struct sockaddr_in);
+      curvec->msg_hdr.msg_iov = msg_iov;
+      curvec->msg_hdr.msg_iovlen = fragCount;
 
-          // Fill the iovector with messages
-          curIov->iov_base = frag->data();     //((char*)frag->buffer)+frag->start;
-          assert((frag->len > 0) && "The UDP protocol allows sending zero length messages but I think you forgot to set the fragment len field.");
-          curIov->iov_len = frag->len;
+      curvec++;
+      msgCount++;
+    } while (next != NULL);
 
-          fragCount++;
-          totalFragCount++;
-          curIov++;
-          } while(nextFrag);
-
-        bzero(&to[msgCount],sizeof(struct sockaddr_in));
-        to[msgCount].sin_family = AF_INET;
-        if (msg->node == Handle::AllNodes)
-          to[msgCount].sin_addr.s_addr= htonl(INADDR_BROADCAST);
-        else
-          to[msgCount].sin_addr.s_addr= htonl(((Udp*)transport)->netAddr | msg->node);
-        to[msgCount].sin_port=htons(msg->port + SAFplusI::UdpTransportStartPort);
-
-        curvec->msg_hdr.msg_controllen = 0;
-        curvec->msg_hdr.msg_control = NULL;
-        curvec->msg_hdr.msg_flags = 0;
-        curvec->msg_hdr.msg_name = &to[msgCount];
-        curvec->msg_hdr.msg_namelen = sizeof (struct sockaddr_in);
-        curvec->msg_hdr.msg_iov = msg_iov;
-        curvec->msg_hdr.msg_iovlen = fragCount;
-
-        curvec++;
-        msgCount++;
-        } while (next != NULL);
-
-
-      int retval = sendmmsg(sock, &msgvec[0], msgCount, 0);  // TODO flags
-      if (retval == -1)
+    int clientSock = -1;
+    // Get the client socket connected to server socket whose information is provided in origMsg argument
+    if (msg->node == Handle::AllNodes) // Send the message to all nodes: get all the client sockets
+    {
+      /*
+      //Loop thru the socket map
+      for(NodeIDSocketMap::iterator iter = clientSockMap.begin(); iter != clientSockMap.end(); iter++)
+      {
+        NodeIDSocketMap::value_type vt = *iter;
+        clientSock = vt.second;
+      }*/
+      //TODO: get node list
+      uint_t nodeList[CL_IOC_MAX_NODES]={0}; // If get node list API returns the dynamic list, we don't need to use the static array like this
+      uint_t port = htons(origMsg->port + SAFplusI::SctpTransportStartPort);
+      for (int i=0;i<CL_IOC_MAX_NODES;i++)
+      {
+        if (nodeList[i])
         {
+          clientSock = getClientSocket(nodeList[i], port);
+          int retval = sendmmsg(clientSock, &msgvec[0], msgCount, 0);  // TODO flags
+          if (retval == -1)
+          {
+            int err = errno;
+            throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
+          }
+          else
+          {
+            assert(retval == msgCount);  // TODO, retry if all messages not sent
+          }    
+        }
+      }
+    }
+    else
+    {
+      clientSock = getClientSocket(origMsg->node, origMsg->port);
+      int retval = sendmmsg(clientSock, &msgvec[0], msgCount, 0);  // TODO flags
+      if (retval == -1)
+      {
         int err = errno;
         throw Error(Error::SYSTEM_ERROR,errno, strerror(errno),__FILE__,__LINE__);
-        }
-      else
-        {
-        assert(retval == msgCount);  // TODO, retry if all messages not sent
-        printf("%d messages sent\n", retval);
-        }
-
-      // Your send routine releases the message whenever you are ready to do so
-      MsgPool* temp = msg->msgPool;
-      temp->free(msg);
       }
-
-#if 0
-tv.tv_sec = 10; /* seconds */
-tv.tv_usec = 0;
-
-if(setsockopt(socket, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) < 0)
-        printf("Cannot Set SO_SNDTIMEO for socket\n");
-
-if(setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
-        printf("Cannot Set SO_RCVTIMEO for socket\n");
-#endif
-
-//#define BUFSIZE 300
-    Message* UdpSocket::receive(uint_t maxMsgs,int maxDelay)
+      else
       {
+        assert(retval == msgCount);  // TODO, retry if all messages not sent
+        //printf("%d messages sent\n", retval);
+      }
+    }
+    
+    // Your send routine releases the message whenever you are ready to do so
+    MsgPool* temp = origMsg->msgPool;
+    temp->free(origMsg);
+  }
+
+  Message* SctpSocket::receive(uint_t maxMsgs, int maxDelay)
+  {
 
       // TODO receive multiple messages
       Message* ret = msgPool->allocMsg();
-      MsgFragment* frag = ret->append(SAFplusI::UdpTransportMaxMsgSize);
+      MsgFragment* frag = ret->append(SAFplusI::SctpTransportMaxMsgSize);
 
-      mmsghdr msgs[SAFplusI::UdpTransportMaxMsg];  // We are doing this for perf so we certainly don't want to new or malloc it!
-      struct iovec iovecs[SAFplusI::UdpTransportMaxFragments];
+      mmsghdr msgs[SAFplusI::SctpTransportMaxMsg];  // We are doing this for perf so we certainly don't want to new or malloc it!
+      struct sockaddr_in from[SAFplusI::SctpTransportMaxMsg];
+      struct iovec iovecs[SAFplusI::SctpTransportMaxFragments];
       struct timespec timeoutMem;
       struct timespec* timeout;
       uint_t flags = MSG_WAITFORONE;
@@ -293,6 +395,8 @@ if(setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
         iovecs[i].iov_len          = frag->allocatedLen;
         msgs[i].msg_hdr.msg_iov    = &iovecs[i];
         msgs[i].msg_hdr.msg_iovlen = 1;
+        msgs[i].msg_hdr.msg_name    = &from[i];
+        msgs[i].msg_hdr.msg_namelen = sizeof(struct sockaddr_in);
         }
 
       int retval = recvmmsg(sock, msgs, 1, flags, timeout);
@@ -318,6 +422,12 @@ if(setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
         for (int msgIdx = 0; (msgIdx<retval); msgIdx++,cur = cur->nextMsg)
           {
           int msgLen = msgs[msgIdx].msg_len;
+          struct sockaddr_in* srcAddr = (struct sockaddr_in*) msgs[msgIdx].msg_hdr.msg_name;
+          assert(msgs[msgIdx].msg_hdr.msg_namelen == sizeof(struct sockaddr_in));
+          assert(srcAddr);
+          
+          cur->port = ntohs(srcAddr->sin_port) - SAFplusI::UdpTransportStartPort;
+          cur->node = ntohl(srcAddr->sin_addr.s_addr) & 255;
           MsgFragment* curFrag = cur->firstFragment;
           for (int fragIdx = 0; (fragIdx < msgs[msgIdx].msg_hdr.msg_iovlen) && msgLen; fragIdx++,curFrag=curFrag->nextFragment)
             {
@@ -337,22 +447,16 @@ if(setsockopt(socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
         }
 
       return ret;
-      }
-
-  static Udp api;
-  };
-
-
-extern "C" SAFplus::ClPlugin* clPluginInitialize(uint_t preferredPluginVersion)
-  {
-  // We can only provide a single version, so don't bother with the 'preferredPluginVersion' variable.
-
-  // Initialize the pluginData structure
-  SAFplus::api.pluginId         = SAFplus::CL_MSG_TRANSPORT_PLUGIN_ID;
-  SAFplus::api.pluginVersion    = SAFplus::CL_MSG_TRANSPORT_PLUGIN_VER;
-  SAFplus::api.type = "UDP";
-
-  // return it
-  return (SAFplus::ClPlugin*) &SAFplus::api;
-  }
-#endif
+   }
+   SctpSocket::~SctpSocket()
+   {
+     // Close all the socket: both server socket itself and client sockets in the map
+     close(sock);
+     for(NodeIDSocketMap::iterator iter = clientSockMap.begin(); iter != clientSockMap.end(); iter++)
+     {
+       NodeIDSocketMap::value_type vt = *iter;
+       int socket = vt.second;
+       close(socket);
+     }
+   }
+};
