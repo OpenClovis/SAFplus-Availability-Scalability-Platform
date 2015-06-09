d6b98a582aedf8d9033ff5369363d9abb0f8d341
Andrew Stone <stone@openclovis.com>
2015-06-09 19:40:51 -0400

    add cleanup function to test execution

--- src/group/test/test.py
+++ src/group/test/test.py
@@ -9,5 +9,5 @@ class test(testcase.TestGroup):
         \brief     	Group functional testss
         """
         # pdb.set_trace()
-        self.progTest(self.model.cfg.mapping.SysCtrl0.installDir + "/test/testGroup",20)  # An App Test just starts running its tests when started (there is no addtl trigger required to put the entity "in service", etc.  The parameter is how long to wait before assuming the test hung.
+        self.progTest(self.model.cfg.mapping.SysCtrl0.installDir + "/test/testGroup",60)  # An App Test just starts running its tests when started (there is no addtl trigger required to put the entity "in service", etc.  The parameter is how long to wait before assuming the test hung.
         self.assert_equal(1, 1, 'This test always works')
