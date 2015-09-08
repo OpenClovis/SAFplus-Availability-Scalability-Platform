ab49c3eac981d61a15bfe499f3b0ede9bb1a98e7
Andrew Stone <stone@openclovis.com>
2015-09-08 12:10:49 -0400

    fix tae run on AMF with new db name and rename test111 to embTest111 so that this file is not checked for test cases on the TAE machine (vs the SAFplus target)

--- src/amf/test/test.py
+++ src/amf/test/test.py
@@ -8,6 +8,6 @@ class test(testcase.TestGroup):
         \testcase   AMF-FNC-TST.TC111
         \brief     	Availability management framework functional 1 node 1 sg 1 comp
         """
-        self.progTest("python {0}/lib/dbalpy.py -x {0}/test/SAFplusAmf1Node1SG1Comp.xml SAFplusAmf".format(self.model.cfg.mapping.SysCtrl0.installDir))
-        self.progTest("python " + self.model.cfg.mapping.SysCtrl0.installDir + "test111.py")
+        self.progTest("python {0}/bin/dbalpy.py -x {0}/test/SAFplusAmf1Node1SG1Comp.xml safplusAmf".format(self.model.cfg.mapping.SysCtrl0.installDir),30)
+        self.progTest("python " + self.model.cfg.mapping.SysCtrl0.installDir + "embTest111.py",200)
         self.assert_equal(1, 1, 'This test always works')
