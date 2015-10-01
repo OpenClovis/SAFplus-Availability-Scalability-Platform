67acc7cfb43bcc38ed32002c29c081225e25b7e6
Andrew Stone <stone@openclovis.com>
2015-10-01 02:49:59 -0400

    refuse to enter nonexistent directory and properly create canonical directory from the current directory and an argument

--- src/python/localaccess.py
+++ src/python/localaccess.py
@@ -4,6 +4,10 @@ import pdb
 mgtGet = safplus.mgtGet
 
 def isValidDirectory(path):
+  data = mgtGet(str("{d=0}"+path))
+  if not data: return False  # TODO, what about an empty list?
+  # print "isValidDir: ", str(data)
+  # pdb.set_trace()
   return True
 
 class Commands:
@@ -11,31 +15,50 @@ class Commands:
     self.commands = {}
     self.context = None
 
+  def canonicalPath(self,location):
+    print "cur: ", self.context.curdir, " loc: ", location
+    if location == "." or location == "" or location == None:
+      return self.context.curdir
+    if location[0] == "/": # not a relative path
+      return location
+    if self.context.curdir == "/":
+      return "/" + location
+    elif self.context.curdir:
+      return self.context.curdir + "/" + location
+    return location
+ 
+
   def do_set(self, location, value):
     """syntax: 
         set (location) (value)  
         Sets the leaf at the specified locations to the specified value
     """
-    loc = self.context.curdir + "/" + location
-    safplus.mgtSet(str(loc),str(value))
-    return ""
+    loc = self.canonicalPath(location)
+    ret = safplus.mgtSet(str(loc),str(value))
+    if ret == 4:  # CL_ERROR_NOT_EXIST
+      return "<error>location [%s] does not exist</error>" % location
+    elif ret == 0:
+      return ""
+    return str(ret)
 
   def do_create(self,location):
     """syntax: 
          create (location)  
          Creates a new object at the specified location.  The type of the object is implied by its location in the tree.
     """
-    loc = self.context.curdir + "/" + location
+    loc = self.canonicalPath(location)
+    print "create " + loc
     safplus.mgtCreate(str(loc))
     return ""
 
-  def do_delete(self,location):
+  def do_delete(self,*locations):
     """syntax: 
          delete (location)  
          deletes the object at (location) and all children
     """
-    loc = self.context.curdir + "/" + location
-    safplus.mgtDelete(str(loc))
+    for l in locations:
+      loc = self.canonicalPath(l)
+      safplus.mgtDelete(str(loc))
     return ""
 
   def setContext(self,context):
