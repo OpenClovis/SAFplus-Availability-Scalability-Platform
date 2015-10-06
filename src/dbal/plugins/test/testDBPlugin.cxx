a4baf0df11ece64aa4da2c9d3ae4c111895117a7
hungta <hungtath99@gmail.com>
2015-10-06 09:53:06 +0700

    Apply c++ plugin framework for dbal7

---
+++ src/dbal/plugins/test/testDBPlugin.cxx
@@ -0,0 +1,63 @@
+#include <clDbalBase.hxx>
+//#include <clPluginApi.hxx>
+
+using namespace SAFplus;
+
+int main()
+{
+  logEchoToFd = 1;
+  SAFplus::logSeverity = SAFplus::LOG_SEV_MAX;
+  safplusInitialize(SAFplus::LibDep::LOG|SAFplus::LibDep::UTILS); 
+  const char* s = "libclSQLiteDB.so";
+  
+  ClPluginHandle* plug = clLoadPlugin(CL_DBAL_PLUGIN_ID,CL_DBAL_PLUGIN_VER,s);
+  if (plug)
+  {
+     DbalPlugin* p = dynamic_cast<DbalPlugin*> (plug->pluginApi);
+     if (p)
+     {
+        //open(ClDBFileT dbFile, ClDBNameT dbName, ClDBFlagT dbFlag, ClUint32T maxKeySize, ClUint32T maxRecordSize
+        ClRcT rc = p->open("/tmp/testDB1.db", "/tmp/testDB1.db", CL_DB_APPEND, 255, 5000);
+        if (rc != CL_OK)
+        {
+          logError("DBAL","TEST", "open database fail");
+          return 0;
+        } 
+        logNotice("DBAL","TEST", "open database OK");
+        const char* key = "KEY1";
+        const char* val = "Value test of KEY1";
+        rc = p->replaceRecord((ClDBKeyT)key, strlen(key)+1, (ClDBRecordT)val, strlen(val)+1);
+        if (rc != CL_OK)
+        {
+          logError("DBAL","TEST","replacing record with key [%s] fail", key);
+          return 0;
+        }
+        ClDBRecordT rec;
+        ClUint32T recSz;
+        rc = p->getRecord((ClDBKeyT)key, strlen(key)+1,&rec, &recSz);
+        if (rc != CL_OK)
+        {
+          logError("DBAL","TEST","getting record with key [%s] fail", key);
+          return 0;
+        }
+        logInfo("DBAL","TEST","Got value [%s]; size [%u]", (char*)rec, recSz);
+        p->freeRecord(rec);
+        rc = p->deleteRecord((ClDBKeyT)key, strlen(key)+1);
+        if (rc != CL_OK)
+        {
+          logError("DBAL","TEST","deleting record with key [%s] fail", key);
+          return 0;
+        }
+        rc = p->getRecord((ClDBKeyT)key, strlen(key)+1,&rec, &recSz);
+        if (rc != CL_OK)
+        {
+          logInfo("DBAL","TEST","Record with key [%s] gotten fail because it was deleted before", key);
+        }
+     }
+  }
+  else
+  {
+    logError("DBAL","TEST", "load plugin fail");
+  }
+  return 1;
+}
---
+++ src/include/clDbalBase.hxx
@@ -0,0 +1,72 @@
+#pragma once
+#include <clDbalApi.h>
+#include <clPluginApi.hxx>
+
+#define NULL_CHECK(X) if(NULL == (X)) { \
+        errorCode = CL_RC(CL_CID_DBAL, CL_ERR_NULL_POINTER); \
+        /*clDbgCodeError(errorCode, ("Parameter " #X " is NULL!"));*/ \
+                        return(errorCode); \
+                      }
+
+/* base class for all dbal plugins including sqlite, gdbm, berkeley and even checkpoint */
+namespace SAFplus {
+
+enum
+ {
+    CL_DBAL_PLUGIN_ID = 0x13746813,
+    CL_DBAL_PLUGIN_VER = 1
+ };
+
+class DbalPlugin: public SAFplus::ClPlugin
+{
+public:
+
+  const char* type;
+ 
+  virtual ClRcT open(ClDBFileT dbFile, ClDBNameT dbName, ClDBFlagT dbFlag, ClUint32T maxKeySize, ClUint32T maxRecordSize)=0;  
+  virtual ClRcT insertRecord(ClDBKeyT dbKey, ClUint32T keySize, ClDBRecordT dbRec, ClUint32T recSize)=0;
+  virtual ClRcT replaceRecord(ClDBKeyT dbKey, ClUint32T keySize, ClDBRecordT dbRec, ClUint32T recSize)=0;
+  virtual ClRcT getRecord(ClDBKeyT dbKey, ClUint32T keySize, ClDBRecordT* pDBRec, ClUint32T* pRecSize)=0;
+  virtual ClRcT getFirstRecord(ClDBKeyT* pDBKey, ClUint32T* pKeySize, ClDBRecordT* pDBRec, ClUint32T* pRecSize)=0;
+  virtual ClRcT getNextRecord(ClDBKeyT currentKey, ClUint32T currentKeySize, ClDBKeyT* pDBNextKey, ClUint32T* pNextKeySize, ClDBRecordT* pDBNextRec, ClUint32T* pNextRecSize)=0;
+  virtual ClRcT deleteRecord(ClDBKeyHandleT dbKey, ClUint32T keySize)=0;
+  virtual ClRcT syncDbal(ClUint32T flags)=0;
+  virtual ClRcT openTransaction(ClDBFileT dbFile, ClDBNameT  dbName, ClDBFlagT dbFlag, ClUint32T  maxKeySize, ClUint32T  maxRecordSize)=0;
+  virtual ClRcT beginTransaction()=0;
+  virtual ClRcT commitTransaction()=0;
+  virtual ClRcT abortTransaction()=0;
+  virtual ClRcT freeRecord(ClDBRecordT dbRec)=0;
+  virtual ClRcT freeKey(ClDBRecordT dbKey)=0;
+  virtual ~DbalPlugin()=0;
+
+  DbalPlugin(DbalPlugin const&) = delete; 
+  DbalPlugin& operator=(DbalPlugin const&) = delete;
+  
+protected:
+  ClDBHandleT pDBHandle;
+
+  virtual ClRcT close()=0; /* destructor of derived class will call this function */
+  DbalPlugin() {}
+};
+
+DbalPlugin::~DbalPlugin() { }
+
+};
+/* specific database classes implement their own functionalities */
+/*
+class SqlitePlugin: public DbalPlugin
+{
+};
+
+class GdbmPlugin: public DbalPlugin
+{
+};
+
+class BerkeleyPlugin: public DbalPlugin
+{
+};
+
+class CheckpointPlugin: public DbalPlugin
+{
+};
+*/
