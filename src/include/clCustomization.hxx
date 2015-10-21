80d773ba62340d58f615290bfc0eb8761bdc26f9
sureshgajjala <suresh@openclovis.com>
2015-10-21 07:04:58 -0700

    Initial Check-in for S7_TIPC

--- src/include/clCustomization.hxx
+++ src/include/clCustomization.hxx
@@ -33,12 +33,6 @@ namespace SAFplus
   enum
     {
       MsgTransportAddressMaxLen = 64, //? The maximum number of bytes allowed for any message transport's physical address (i.e. IP address, TIPC address, etc).  This is needed to store node addresses in shared memory when using cloud mode clustering.
-
-      MsgSafplusReservedPorts  = 32,  //? The number of ports reserved for use by SAFplus applications.  Ports reserved are: 0 -> SAFplusReservedPorts-1.  DO NOT CHANGE
-      MsgApplicationPortStart  = MsgSafplusReservedPorts,  //? The "well-known" ports reserved for your use.
-      MsgApplicationPortEnd    = MsgApplicationPortStart + 100,  //? The last "well-known" port reserved for your use
-      MsgDynamicPortStart      = MsgApplicationPortEnd+1,  //? The AMF will dynamically allocate ports when it starts up an application from this group.
-      MsgDynamicPortEnd        = MsgDynamicPortStart+512,  //? The AMF will dynamically allocate ports when it starts up an application from this group.  This is the largest port in the dynamic group.
     };
   };
 
@@ -46,14 +40,6 @@ namespace SAFplus
 /* Configuration parameters that are used internally */
 namespace SAFplusI
   {
-
-  /* Messaging */
-  enum
-    {
-    MsgSafplusSendReplyRetryInterval = 4000,   //? Default time to wait before retrying a message that expects a reply (uses sendReply API)
-    RpcRetryInterval = 4000,  //? Default time to wait before retrying a RPC that expects a reply
-    };
-
   /* THREADS */
   enum
     {
@@ -99,14 +85,6 @@ namespace SAFplusI
     FaultMaxMembers    = 1024,   // Maximum number of fault entity
     };
 
-  /* MGT */
-  enum
-    {
-      MgtToStringRecursionDepth = 64,  //? When converting an object with children to XML this the maximum child depth.  By default this is set to a depth that should exceed any reasonable management hierarchy, but not be infinite in case the MGT tree has a cycle.
-      MgtHistoryArrayLen = 1000 //? How many samples should be kept for the history of MgtHistoryStat statistic objects
-    };
-
-
   /* AMF */
   enum
     {
@@ -119,10 +97,20 @@ namespace SAFplusI
   enum
     {
     UdpTransportMaxMsgSize = 65507,  // 65,535 - 8 byte UDP header - 20 byte IP header  (http://en.wikipedia.org/wiki/User_Datagram_Protocol).  This is defined here so you can artifically limit the packet size.
-    UdpTransportNumPorts = 256,  // Limit the ports to a range for no particular reason
-    UdpTransportStartPort = 17000,  // Pick a random spot in the UDP port range so our ports don't overlap common services
+    UdpTransportNumPorts = 2048,  // Limit the ports to a range for no particular reason
+    UdpTransportStartPort = 7000,  // Pick a random spot in the UDP port range so our ports don't overlap common services
     UdpTransportMaxMsg = 1024,
     UdpTransportMaxFragments = 1024,
+    };
+
+  /* TIPC message transport */
+  enum
+    {
+    TipcTransportMaxMsgSize = 66000,
+    TipcTransportNumPorts = 2048,  // Limit the ports to a range for no particular reason
+    TipcTransportStartPort = 11000,  // Pick a random spot in the TIPC port range so our ports don't overlap common services
+    TipcTransportMaxMsg = 1024,
+    TipcTransportMaxFragments = 1024,
     };
 
   /* SCTP message transport */
@@ -130,7 +118,7 @@ namespace SAFplusI
     {
     SctpTransportMaxMsgSize = 65503,  // 65,535 - 12 bytes SCTP header - 20 bytes IP header. This is defined here so you can artifically limit the packet size.
     SctpTransportNumPorts = 2048,  // Limit the ports to a range for no particular reason
-    SctpTransportStartPort = 18000,  // Pick a random spot in the UDP port range so our ports don't overlap common services
+    SctpTransportStartPort = 8000,  // Pick a random spot in the UDP port range so our ports don't overlap common services
     SctpTransportMaxMsg = 1024,
     SctpTransportMaxFragments = 1024,
     SctpMaxStream = 64,
@@ -140,27 +128,13 @@ namespace SAFplusI
     {
     TcpTransportMaxMsgSize = 65475,  // 65,535 - 40 bytes TCP header - 20 bytes IP header. This is defined here so you can artifically limit the packet size.
     TcpTransportNumPorts = 2048,  // Limit the ports to a range for no particular reason
-    TcpTransportStartPort = 19000,  // Pick a random spot in the TCP port range so our ports don't overlap common services
+    TcpTransportStartPort = 9000,  // Pick a random spot in the TCP port range so our ports don't overlap common services
     TcpTransportMaxMsg = 1024,
-    TcpTransportMaxFragments = 2048,
+    TcpTransportMaxFragments = 1024,
     TcpMaxStream = 64,
     }; 
 
   extern const char* defaultMsgTransport;  //? Specifies the default messaging transport plugin filename.  This can be overridden by an environment variable.
-
-  /* ckpt default retention duration */
-  enum 
-  {
-    CkptUpdateDuration = 60, /* This is the configured duration in second for which the program update the checkpoint, means to get changed checkpoint parameters such as last used time or there is any new checkpoint added */
-    CkptRetentionDurationDefault = 28800, /* This is the default retention duration in second (8*60*60) for the retention timer to decide if a checkpoint data is deleted from memory */
-  };
-
-  /* ckpt peristent parameters */
-  enum 
-  {
-    CkptMaxKeySize = 1000, /* Maximum size of the key (in bytes) to be stored in the database. This parameter is ignored in case of Berkeley and GDBM databases */
-    CkptMaxRecordSize = 40000, /* Maximum size of the record (in bytes) to be stored in the database. This parameter is ignored in case of Berkeley and GDBM databases */
-  };
 
   };
 
