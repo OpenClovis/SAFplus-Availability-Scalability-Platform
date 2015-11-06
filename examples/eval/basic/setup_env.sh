dd5e634cd685f3abc9a99b30666d35339d936c91
Andrew Stone <stone@openclovis.com>
2015-11-06 12:36:21 -0500

    fix directory issue in setup_env.sh

--- examples/eval/basic/setup_env.sh
+++ examples/eval/basic/setup_env.sh
@@ -7,7 +7,7 @@ export SAFPLUS_BACKPLANE_INTERFACE=eth0
 
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SAFPLUS_DIR=/opt/safplus/7.0/sdk
-MA=i686-linux-gnu
+MA=`g++ -dumpmachine`
 export PATH=$MY_DIR/bin:$SAFPLUS_DIR/target/$MA/bin:$PATH
 export LD_LIBRARY_PATH=$MY_DIR:$MY_DIR/lib:$MY_DIR/plugin:$SAFPLUS_DIR/target/$MA/lib::$SAFPLUS_DIR/target/$MA/plugin:$LD_LIBRARY_PATH
 export PYTHONPATH=$MY_DIR/lib:$SAFPLUS_DIR/target/$MA/lib:$PYTHONPATH
