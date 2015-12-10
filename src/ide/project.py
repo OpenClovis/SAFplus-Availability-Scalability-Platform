7b40c14062d075cccb303358b5ac81cc978338ea
hungta <hungtath99@gmail.com>
2015-12-10 14:31:51 +0700

    project file, model file should be created on 'new project'

--- src/ide/project.py
+++ src/ide/project.py
@@ -9,8 +9,10 @@ import wx
 import  wx.lib.newevent
 
 import microdom
+import xml.dom.minidom
 from common import log
 import re
+import ntpath
 
 PROJECT_LOAD = wx.NewId()
 PROJECT_SAVE = wx.NewId()
@@ -25,7 +27,7 @@ class Project(microdom.MicroDom):
     self.dirty = False
     self._safplusModel = None
     self.datamodel = None
-    self.name = ""
+    self.name = ""    
     if filename: self.load(filename)
     pass
 
@@ -33,11 +35,20 @@ class Project(microdom.MicroDom):
     """Returns the location of this model on disk """
     return os.path.dirname(self.projectFilename)   
 
-  def load(self,filename):
+  def modelFilename(self):
+    return self.name+"Model.xml"
+
+  def sourceFilename(self):
+    return self.name+".cpp"
+
+  def headerFilename(self):
+    return self.name+".h"
+
+  def load(self,filename):    
     self.projectFilename = filename
     self.name = os.path.splitext(os.path.basename(filename))[0]
     md = microdom.LoadFile(filename)
-    microdom.MicroDom.__init__(self,md.attributes_, md.children_, md.data_)
+    microdom.MicroDom.__init__(self,md.attributes_, md.children_, md.data_)    
     # TODO validate the structure of the microdom tree
 
   def setSAFplusModel(self,mdl):
@@ -45,14 +56,74 @@ class Project(microdom.MicroDom):
 
   def save(self, filename=None):
     if not filename: filename = self.projectFilename
-    # TODO, save the project XML 
-
+    # Perhaps, the prj xml contents will not be changed, so it should be saved once on new
     # Now save the model to XML
     self._safplusModel.save()
 
-  def new(self, datamodel):
+  def new(self, datamodel):    
     self.name = os.path.splitext(os.path.basename(self.projectFilename))[0]
     self.datamodel = datamodel
+    # project file (.ssp), model (.xml) file should be created. Model file is created with main tags in which data is empty
+    self.createPrjXml()
+    self.createModelXml()
+    md = microdom.LoadFile(self.projectFilename)
+    microdom.MicroDom.__init__(self,md.attributes_, md.children_, md.data_)
+
+  def createPrjXml(self):
+    dom = xml.dom.minidom.Document()
+    safplusPrjElem = dom.createElement("SAFplusProject")
+    dom.appendChild(safplusPrjElem)
+    safplusPrjElem.setAttribute("version", "7.0")
+
+    datamodelElem = dom.createElement("datamodel")
+    safplusPrjElem.appendChild(datamodelElem)
+    datamodel = ntpath.basename(self.datamodel)
+    datamodelText = dom.createTextNode(datamodel)
+    datamodelElem.appendChild(datamodelText)
+
+    modelElem = dom.createElement("model")
+    safplusPrjElem.appendChild(modelElem)
+    modelText = dom.createTextNode(self.modelFilename())
+    modelElem.appendChild(modelText)
+
+    sourceElem = dom.createElement("source")
+    safplusPrjElem.appendChild(sourceElem)
+    sourceText = dom.createTextNode(self.sourceFilename()+" "+self.headerFilename())
+    sourceElem.appendChild(sourceText)
+
+    data = microdom.LoadMiniDom(dom.childNodes[0])
+    f = open(self.projectFilename,"w")
+    f.write(data.pretty())
+    f.close()    
+        
+  def createModelXml(self):
+    """ Create the project model.xml but there is no detail data in it """
+    dom = xml.dom.minidom.Document()
+    safplusModelElem = dom.createElement("SAFplusModel")
+    dom.appendChild(safplusModelElem)
+    safplusModelElem.setAttribute("version", "7.0")
+
+    moduleElem = dom.createElement("modules")
+    safplusModelElem.appendChild(moduleElem)
+    yangElem = dom.createElement("yang")
+    moduleElem.appendChild(yangElem)    
+    datamodel = ntpath.basename(self.datamodel)
+    datamodelText = dom.createTextNode(datamodel)
+    yangElem.appendChild(datamodelText)
+
+    entitiesElem = dom.createElement("entities")
+    safplusModelElem.appendChild(entitiesElem)    
+
+    ideElem = dom.createElement("ide")
+    safplusModelElem.appendChild(ideElem)
+
+    instancesElem = dom.createElement("instances")
+    safplusModelElem.appendChild(instancesElem)        
+
+    data = microdom.LoadMiniDom(dom.childNodes[0])
+    f = open(self.directory()+os.sep+self.modelFilename(),"w")
+    f.write(data.pretty())
+    f.close()  
 
 
 class ProjectTreeCtrl(wx.TreeCtrl):
@@ -136,33 +207,32 @@ class ProjectTreePanel(wx.Panel):
        (child, cookie) = self.tree.GetNextChild(self.root, cookie)
      return False
 
-  def populateGui(self,project,tree, onNew=False):
+  def populateGui(self,project,tree):
      p = project.projectFilename
      projName = os.path.basename(p)
      prjT = self.tree.AppendItem(self.root, projName)
      self.projects.append(prjT)
      self.tree.SetPyData(prjT, project)
      dmT = self.tree.AppendItem(prjT, "datamodel")
-     if not onNew:
-       for ch in project.datamodel.children():
-         for c in ch.split():
-           c = c.strip()
-           fileT = self.tree.AppendItem(dmT, c)
-           self.tree.SetPyData(fileT, (project,c))  # TODO more descriptive PY data   
+     for ch in project.datamodel.children():
+       for c in ch.split():
+         c = c.strip()
+         fileT = self.tree.AppendItem(dmT, c)
+         self.tree.SetPyData(fileT, (project,c))  # TODO more descriptive PY data   
      mdlT = self.tree.AppendItem(prjT, "model")
-     if not onNew:
-       for ch in project.model.children():
-         for c in ch.split():
-           c = c.strip()
-           fileT = self.tree.AppendItem(mdlT,c)
-           self.tree.SetPyData(fileT, (project,c))  # TODO more descriptive PY data   
+     
+     for ch in project.model.children():
+       for c in ch.split():
+         c = c.strip()
+         fileT = self.tree.AppendItem(mdlT,c)
+         self.tree.SetPyData(fileT, (project,c))  # TODO more descriptive PY data   
      srcT = self.tree.AppendItem(prjT, "source")
-     if not onNew:
-       for ch in project.source.children():
-         for c in ch.split():
-           c = c.strip()
-           fileT = self.tree.AppendItem(srcT,c)
-           self.tree.SetPyData(fileT, (project,c))  # TODO more descriptive PY data   
+     
+     for ch in project.source.children():
+       for c in ch.split():
+         c = c.strip()
+         fileT = self.tree.AppendItem(srcT,c)
+         self.tree.SetPyData(fileT, (project,c))  # TODO more descriptive PY data   
  
      # TODO: add children under subdirectories    
 
@@ -195,7 +265,7 @@ class ProjectTreePanel(wx.Panel):
       project = Project()
       project.projectFilename = dlg.path
       project.new(dlg.datamodel.GetValue())
-      self.populateGui(project, self.root, True)
+      self.populateGui(project, self.root)
       evt = ProjectNewEvent(EVT_PROJECT_NEW.evtType[0])
       wx.PostEvent(self.parent,evt)
 
@@ -248,7 +318,7 @@ class NewPrjDialog(wx.Dialog):
         datamodel_sizer.Add(datamodel_lbl, 0, wx.ALL|wx.CENTER, 5)
         self.datamodel = wx.TextCtrl(self, size=(200, 25))
         datamodel_sizer.Add(self.datamodel, 0, wx.ALL, 5)
-        select_btn = wx.Button(self, label="Select")
+        select_btn = wx.Button(self, label="Browse")
         select_btn.Bind(wx.EVT_BUTTON, self.onSelect)
         datamodel_sizer.Add(select_btn, 0, wx.ALL, 5)
  
