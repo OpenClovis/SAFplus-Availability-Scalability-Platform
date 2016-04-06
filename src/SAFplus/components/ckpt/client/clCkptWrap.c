OpenClovis/SAFplus-Availability-Scalability-Platform

commit 48a1cf730da5869ed4a7882ad3e6282aa47109e9

Issue: MSG queue could not create during node failover Soluton: If ckpt handle is still not updated during node failover, delay and retry until it done.

src/SAFplus/components/ckpt/client/clCkptWrap.c
@@ -1529,7 +1529,7 @@ static ClBoolT clCkptHandleTypicalErrors(ClRcT rc, ClCkptHdlT ckptHdl,ClIocNodeA
          (CL_IOC_ERR_HOST_UNREACHABLE == CL_GET_ERROR_CODE(rc)) || 
          (rc == CL_CKPT_ERR_NOT_EXIST && nodeAddr && *nodeAddr == CL_CKPT_UNINIT_VALUE)
          ||
-         rc == CL_ERR_NOT_EXIST)
+         (CL_GET_ERROR_CODE(rc) == CL_ERR_NOT_EXIST))
     {
         /* 
          * Maybe the active address has changed and this client 
@@ -1761,6 +1761,14 @@ ClRcT clCkptSectionCreate(
             }
         }while(CL_GET_ERROR_CODE(rc) == CL_ERR_TIMEOUT && (numRetries++ < 2));                         
         tryAgain = clCkptHandleTypicalErrors(rc, ckptHdl,&nodeAddr);
+
+        /* 
+         * Maybe the active address has changed and this client hasn't received the update yet.
+         */
+        if ((rc != CL_OK) && (nodeAddr == CL_CKPT_UNINIT_VALUE))
+        {
+            rc = CL_ERR_TRY_AGAIN;
+        }
 
     }while((tryAgain == CL_TRUE) && (maxRetry++ < 2));
 

src/SAFplus/components/msg/server/clMsgCkptServer.c
@@ -366,6 +366,11 @@ ClRcT clMsgQCkptDataUpdate(ClMsgSyncActionT syncupType, ClMsgQueueCkptDataT *pQu
             else
                 rc = clCacheEntryDelete(&gMsgQCkptServer, &ckptData.sectionName);
 
+            if (CL_GET_ERROR_CODE(rc) == CL_ERR_NOT_EXIST)
+            {
+              rc = CL_OK; //It is OK if ckpt has been deleted in node failover
+            }
+
             clMsgQueueGroupsRemove((ClNameT *)&ckptData.sectionName);
             break;
         case CL_MSG_DATA_UPD:

commit 5fcfeb89e285c15dd907ba7d03949ad2320df53c

Fixed wrong behavior with 'try again' caused by commit 48a1cf7

src/SAFplus/components/ckpt/client/clCkptWrap.c
@@ -1762,6 +1762,7 @@ ClRcT clCkptSectionCreate(
         }while(CL_GET_ERROR_CODE(rc) == CL_ERR_TIMEOUT && (numRetries++ < 2));                         
         tryAgain = clCkptHandleTypicalErrors(rc, ckptHdl,&nodeAddr);
 
+#if 0
         /* 
          * Maybe the active address has changed and this client hasn't received the update yet.
          */
@@ -1769,7 +1770,7 @@ ClRcT clCkptSectionCreate(
         {
             rc = CL_ERR_TRY_AGAIN;
         }
-
+#endif
     }while((tryAgain == CL_TRUE) && (maxRetry++ < 2));
 
     /* 