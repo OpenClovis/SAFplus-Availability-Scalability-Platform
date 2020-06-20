f1d90e3838babc96d8a4311beaf60de1b1b0793c
tuyen.ho <tuyen.ho@terralogic.com>
2020-06-20 11:38:58 +0700

    Update the missing file

---
+++ src/SAFplus/components/ioc/client/clIocDebugCli.h
@@ -0,0 +1,11 @@
+#include <clCommon.h>
+
+extern ClRcT clIocDebugCliTransportMcastPeerListAdd(int argc, 
+        char         **argv ,
+        ClCharT      **ret);
+extern ClRcT clIocDebugCliTransportMcastPeerListDelete(int argc, 
+        char         **argv ,
+        ClCharT      **ret);
+extern ClRcT clIocDebugCliTransportMcastPeerListGet(int argc, 
+        char         **argv ,
+        ClCharT      **ret);
