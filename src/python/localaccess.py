57f9e8e5ff77b580d9281c409e5d1cf25d0e9a9a
tuyen.ho <tuyen.ho@terralogic.com>
2020-11-13 13:09:27 +0700

    Set condition to do_entitySet

--- src/python/localaccess.py
+++ src/python/localaccess.py
@@ -3,6 +3,9 @@ import safplus
 import pdb
 
 mgtGet = safplus.mgtGet
+mgtSet = safplus.mgtSet
+mgtCreate = safplus.mgtCreate
+mgtDelete = safplus.mgtDelete
 getProcessHandle = safplus.getProcessHandle
 
 amfMgmtInitialize = safplus.amfMgmtInitialize
@@ -98,80 +101,6 @@ class Commands:
   def setContext(self,context):
     """Sets the context (environment) object so commands can access it while they are executing"""
     self.context = context
-
-  def do_entitySet(self, entityType, entityName, firstAttribute, firstValue, *attributesAndValuesPairs):
-    """syntax: entitySet entityType entityName attribute value
-    Set the specified values to the attributes
-    """
-    if len(attributesAndValuesPairs) % 2 != 0:
-      print "don't have values with the corresponding attributes"
-      return ""
-
-    t = "/safplusAmf" + "/" + entityType + "/" + entityName
-
-    listAttributesAndValuesPairs = list(attributesAndValuesPairs)
-    listAttributesAndValuesPairs.append(firstAttribute)
-    listAttributesAndValuesPairs.append(firstValue)
-
-    i = 0
-    while i < len(listAttributesAndValuesPairs):
-      loc = self.canonicalPath(t + "/" + listAttributesAndValuesPairs[i])
-      try:
-        ret = safplus.mgtSet(str(loc),str(listAttributesAndValuesPairs[i+1]))
-      except RuntimeError, e:
-        print "<error>location [%s] error [%s]</error>" % (location, str(e))
-      i += 2
-      if ret == 4:  # CL_ERROR_NOT_EXIST
-        print "<error>location [%s] does not exist</error>" % loc
-    return ""
-
-  def do_entityCreate(self, *entityTypeAndEntityNamePairs):
-    """syntax: entityCreate entityType entityName
-    Creates a new entityName with the specified entityType
-    """
-
-    if len(entityTypeAndEntityNamePairs) % 2 != 0:
-      print "don't have entityName with the corresponding entityType"
-      return ""
-
-    t = "/safplusAmf"
-    i = 0
-    while i < len(entityTypeAndEntityNamePairs):
-      entityType = entityTypeAndEntityNamePairs[i]
-      entityName = entityTypeAndEntityNamePairs[i+1]
-      loc = self.canonicalPath(t + "/" + entityType + "/" + entityName)
-      try:
-        ret = safplus.mgtCreate(str(loc))
-      except RuntimeError, e:
-        print"<error>location [%s] error [%s]</error>" % (location, str(e))
-      i += 2
-      if ret == 16:  # CL_ERR_ALREADY_EXIST
-        print "<error>entityName [%s] already exists</error>" % entityName
-    return ""
-
-  def do_entityDelete(self, *entityTypeAndEntityNamePairs):
-    """syntax: entityDelete entityType entityName
-    Deletes the entityNames
-    """
-
-    if len(entityTypeAndEntityNamePairs) % 2 != 0:
-      print "don't have entityName with the corresponding entityType"
-      return ""
-
-    t = "/safplusAmf"
-    i = 0
-    while i < len(entityTypeAndEntityNamePairs):
-      entityType = entityTypeAndEntityNamePairs[i]
-      entityName = entityTypeAndEntityNamePairs[i+1]
-      loc = self.canonicalPath(t + "/" + entityType + "/" + entityName)
-      try:
-        ret = safplus.mgtDelete(str(loc))
-      except RuntimeError, e:
-        print"<error>location [%s] error [%s]</error>" % (location, str(e))
-      i += 2
-      if ret == 19:  # CL_ERR_DOESNT_EXIST
-        print "<error>entityName [%s] does not exist</error>" % entityName
-    return ""
 
 def Initialize():
   svcs = safplus.Libraries.MSG | safplus.Libraries.GRP | safplus.Libraries.MGT_ACCESS
