78bcaf9ed7b4fc0a0bbe34d27dd452288f3f2918
hungta <hungtath99@gmail.com>
2022-01-11 14:12:52 +0700

    make changes to be compatiable and work with Ubuntu 20.04

--- src/SAFplus/mk/make-common.mk
+++ src/SAFplus/mk/make-common.mk
@@ -121,7 +121,7 @@ endif
 # To verify gcc
 GCCVERSIONGTEQ4 := $(shell expr `gcc -dumpversion | cut -f1 -d.` \>= 7)
 ifeq "$(GCCVERSIONGTEQ4)" "1"
-    CFLAGS += -Wno-error=format-truncation -Wno-error=format-overflow -Wno-stringop-overflow -Wno-restrict -Wno-stringop-truncation -Wno-array-bounds
+    CFLAGS += -Wno-error=format-truncation -Wno-error=format-overflow -Wno-stringop-overflow -Wno-restrict -Wno-stringop-truncation -Wno-array-bounds -Wno-address-of-packed-member
 #    $(warning Greater than or equal to GCCVERSION g: $(CFLAGS))
 endif
 
