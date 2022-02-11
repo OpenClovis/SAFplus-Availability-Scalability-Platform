f0182694210607208aa394a6dc64c74bb18f573b
win <tuyen.ho@terralogic.com>
2022-02-11 09:50:26 +0700

    Develop some funtions for ARD

--- src/python/textxmlterm.py
+++ src/python/textxmlterm.py
@@ -94,7 +94,6 @@ class XmlResolver:
         if out[0].__name__ == "handleRpc":
           return (out,lst,None)  # TODO keyword args
         else:
-          print"bbb"
           return (out,lst[1:],None)  # TODO keyword args
 
     default = cmds.get(None,None)  # If there's a dictionary entry whose key is None, then this is used if nothing else matches
